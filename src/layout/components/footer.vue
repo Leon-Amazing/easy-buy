15a15984f32834f62803f9b2de801a8fa964d86b
wangdouliang <wangdouliang@zzltop.com>
2024-11-29 16:29:49 +0800

    feat: header/footer

--- src/layout/components/footer.vue
+++ src/layout/components/footer.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="item h-40vh bg-#f9f5f0">footer</div>
+  <div class="item h-40vh bg-#4a4640 flex justify-around items-center">
+    <div class="left">
+      <div class="title font-size-35px font-weight-bold c-white m-b-10px">objects</div>
+      <div class="des font-size-20px c-white m-b-16px max-w-20vw">The most natural, care-free and easy to use objects that you can get.</div>
+      <div class="social-media flex">
+        <img src="/img/facebook.png" class="w-30px m-r-10px cursor-pointer" @click="handleClick('https://www.google.com/')" />
+        <img src="/img/instagram.png" class="w-30px m-r-10px cursor-pointer" @click="handleClick('https://www.google.com/')" />
+        <img src="/img/twiter.png" class="w-30px cursor-pointer" @click="handleClick('https://www.google.com/')" />
+      </div>
+    </div>
+    <div class="qr-code">
+      <div class="title font-size-35px font-weight-bold c-white m-b-10px">QR code</div>
+      <div class="social-media flex">
+        <img src="/img/qr-code.png" class="w-100px m-r-20px cursor-pointer" />
+        <img src="/img/qr-code.png" class="w-100px m-r-20px cursor-pointer" />
+        <img src="/img/qr-code.png" class="w-100px cursor-pointer" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+function handleClick(url: string) {
+  window.open(url, '_blank');
+}
+</script>
 
 <style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="home-wrapper">
+    <div class="header h-8vh position-absolute top-0 left-0 right-0 z-10 c-white flex justify-between items-center px-20vw b-b-0.1px border-color-#e2d5c5" :class="{ sticky: isSticky }">
+      <div class="headline cursor-pointer">EASY BUY</div>
+      <div class="navs flex justify-between items-center cursor-pointer">
+        <div class="navs-item m-r-20px" @click="scrollToAnchor('section1')">product1</div>
+        <div class="navs-item m-r-20px" @click="scrollToAnchor('section2')">product2</div>
+        <div class="navs-item" @click="scrollToAnchor('section3')">product3</div>
+      </div>
+    </div>
     <swiper />
-    <product-one />
-    <product-two />
-    <advertise />
-
+    <product-one id="section1" />
+    <advertise id="section2" />
+    <product-two id="section3" />
     <div class="to-top" @click="scrollToTop" v-show="showScrollButton">↑</div>
   </div>
 </template>
@@ -17,13 +24,28 @@ import Advertise from '@/components/Advertise.vue';
 import { onBeforeUnmount, onMounted, ref } from 'vue';
 
 const showScrollButton = ref(false);
+const isSticky = ref(false);
+let lastScrollY = 0;
+
+const scrollToAnchor = (id: string) => {
+  const element = document.getElementById(id) as HTMLElement;
+  element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+};
 
 function handleScroll() {
-  if (window.scrollY > 200) {
+  const currentScrollY = window.scrollY;
+  // 判断是否向上滑动
+  if (currentScrollY < lastScrollY && currentScrollY > 0) {
+    isSticky.value = true; // 向上滑动，设置为 sticky
+  } else {
+    isSticky.value = false; // 向下滑动，取消 sticky
+  }
+  if (currentScrollY > 200) {
     showScrollButton.value = true;
   } else {
     showScrollButton.value = false;
   }
+  lastScrollY = currentScrollY;
 }
 
 function scrollToTop() {
@@ -44,6 +66,30 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 .to-top {
-  @apply fixed bottom-20px right-20px z-5 bg-#4a4640 color-#ffffff font-size-18px px-12px py-3px cursor-pointer;
+  @apply fixed bottom-20px right-20px z-5 bg-#888888 color-#ffffff font-size-18px px-12px py-3px cursor-pointer rounded-5px;
+}
+.header {
+  transition: top 0.3s ease;
+  &.sticky {
+    animation: showHead 0.3s ease;
+    @apply position-sticky bg-#f9f5f0 c-#333941;
+
+    .headline,
+    .navs-item {
+      @apply hover:c-#000000;
+    }
+  }
+}
+.headline,
+.navs-item {
+  @apply hover:c-#e2d5c5;
+}
+@keyframes showHead {
+  0% {
+    top: -10vh;
+  }
+  100% {
+    top: 0;
+  }
 }
 </style>
